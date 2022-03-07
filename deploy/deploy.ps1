9841dbac99989aaa78cd55d8eab6a3b479722056
AzureAD\MevlütKahraman(Deleg) <mka@delegate.dk>
2022-03-07 10:28:22 +0100

    updated content

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,9 +1,9 @@
 $outputSubscription = az account set --subscription cf2a46ee-d0b3-4e72-8cb8-66572c7f0313
 
-Throw-WhenError -output $output
+Throw-WhenError -output $outputSubscription
 
 $outputRG = az group create `
     --name "DevOps" `
     --location "eastus" 
 
-Throw-WhenError -output $output
+Throw-WhenError -output $outputRG
